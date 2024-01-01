799a71cebea2966ef7b92d31d2f7c70906acd9ce
Hello Phone <85028651+hello1234316@users.noreply.github.com>
2024-01-01 20:27:12 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,3 +39,21 @@ document.addEventListener("DOMContentLoaded", function(){
 	;
 
 })
+
+$(document).ready(function() {
+  $('#menu-toggle, #menu > ul > li > a').click(function(e) { 
+      var $toggle = $(this); 
+      var $menu = $('#' + $(this).attr('aria-controls')); 
+  
+      if ($menu.attr('aria-hidden') == 'true') {
+          $('body').addClass('open');
+          $menu.attr('aria-hidden', 'false');
+          $toggle.attr('aria-expanded', 'true');
+      }
+      else if ($menu.attr('aria-hidden') == 'false') {
+          $('body').removeClass('open');
+          $menu.attr('aria-hidden', 'true');
+          $toggle.attr('aria-expanded', 'faremove'); 
+      }
+  });
+});
